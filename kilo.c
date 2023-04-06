d2dbd0706520b789026c3b589d96f4443977cf42
0Vipin0 <vipinmalik941@gmail.com>
2023-04-06 20:19:32 +0530

    Step 5 - Added Comments

--- kilo.c
+++ kilo.c
@@ -4,13 +4,18 @@
 void enableRawMode(){
 	struct termios raw;
 	
-	// Get the Terminal Data in to raw;
+	// Get the Terminal Data in to raw
+	// Linux terminal input, output and errro mode
+	// STDIN_FILENO -> 0
+	// STDOUT_FILENO -> 1
+	// STDERR_FILENO -> 2
 	tcgetattr(STDIN_FILENO, &raw);
 
 	// Turn off Echo
 	raw.c_lflag &= ~(ECHO);
 	
 	// Set the Terminal Data
+	// TCSAFLUSH argument specifies when to apply the change, it waits for all pending output to be written to the terminal, discard any input that hasn't been read
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
