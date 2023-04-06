34e43cb0bb936cfb341b5578fea5ebcecfec9e2e
0Vipin0 <vipinmalik941@gmail.com>
2023-04-06 19:56:41 +0530

    Step 4

--- kilo.c
+++ kilo.c
@@ -4,6 +4,6 @@ int main() {
 	// Variable to get the input
 	char c;
 	// Read 1 byte from the standard input into 'c' until no bytes left to read or q key is entered
-	while (read(STDIN_FILENO, &c, 1) == 1 && c !- 'q');
+	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
 	return 0;
 }
