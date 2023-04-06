c7af84ee4ec61c12d86ff3342917d162666735c8
0Vipin0 <vipinmalik941@gmail.com>
2023-04-06 19:54:47 +0530

    Step 3

--- kilo.c
+++ kilo.c
@@ -1,3 +1,9 @@
+#include <unistd.h>
+
 int main() {
+	// Variable to get the input
+	char c;
+	// Read 1 byte from the standard input into 'c' until no bytes left to read
+	while (read(STDIN_FILENO, &c, 1) == 1);
 	return 0;
 }
