7e4b763d7c4ef2761ae762ac44bf9ba2b003aeab
0Vipin0 <vipinmalik941@gmail.com>
2023-04-06 20:03:08 +0530

    Step 5 - Turn off Echo

--- kilo.c
+++ kilo.c
@@ -1,6 +1,22 @@
+#include <termios.h>
 #include <unistd.h>
 
+void enableRawMode(){
+	struct termios raw;
+	
+	// Get the Terminal Data in to raw;
+	tcgetattr(STDIN_FILENO, &raw);
+
+	// Turn off Echo
+	raw.c_lflag &= ~(ECHO);
+	
+	// Set the Terminal Data
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
 int main() {
+	// Turn the terminal to Raw mode from Canonical Mode
+	enableRawMode();
 	// Variable to get the input
 	char c;
 	// Read 1 byte from the standard input into 'c' until no bytes left to read or q key is entered
