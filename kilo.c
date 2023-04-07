de4783b40c4a6c582daa18dc4d834390118bb745
0Vipin0 <vipinmalik941@gmail.com>
2023-04-07 19:57:48 +0530

    Step 16 - Timeout for read()

--- kilo.c
+++ kilo.c
@@ -30,6 +30,10 @@ void enableRawMode(){
 	raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
 	// Bit Mask - Set character size to 8 bits 
 	raw.c_cflag |= (CS8);
+	// VMIN - Set Minimum Number of bytes of input needed before read() return
+	raw.c_cc[VMIN] = 0;
+	// VMAX - Set Maximum Time amount to wait before read() returns
+	raw.c_cc[VTIME] = 1;
 
 	// Set the Terminal Data
 	// TCSAFLUSH argument specifies when to apply the change, it waits for all pending output to be written to the terminal, discard any input that hasn't been read
@@ -39,15 +43,17 @@ void enableRawMode(){
 int main() {
 	// Turn the terminal to Raw mode from Canonical Mode
 	enableRawMode();
-	// Variable to get the input
-	char c;
 	// Read 1 byte from the standard input into 'c' until no bytes left to read or q key is entered
-	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q'){
+	while (1){
+		// Variable to get the input
+		char c = '\0';
+		read(STDIN_FILENO, &c, 1);
 		if(iscntrl(c)){
 			printf("%d\r\n", c);
 		} else {
 			printf("%d ('%c')\r\n", c, c);
 		}
+		if (c == 'q') break;
 	}
 	return 0;
 }
