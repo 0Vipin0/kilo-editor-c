a635c4832ec17484a023126673546836e0f9361a
0Vipin0 <vipinmalik941@gmail.com>
2023-04-11 21:23:57 +0530

    Step 30 - Get Cursor Position using Terminal Commands

--- kilo.c
+++ kilo.c
@@ -82,6 +82,25 @@ char editorReadKey(){
 	return c;
 }
 
+int getCursorPostion(int *rows, int *cols){
+	if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+
+	printf("\r\n");
+
+	char c;
+	while (read(STDIN_FILENO, &c, 1) == 1){
+		if (iscntrl(c)){
+			printf("%d\r\n", c);
+		} else {
+			printf("%d ('%c')\r\n", c, c);
+		}
+	}
+
+	editorReadKey();
+
+	return -1;
+}
+
 int getWindowSize(int *rows, int *cols){
 	// From sys/ioctl
 	struct winsize ws;
@@ -89,8 +108,7 @@ int getWindowSize(int *rows, int *cols){
 	if (1 || ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0){
 		// C -> moves cursor to right, B -> moves cursor to down
 		if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
-		editorReadKey();
-		return -1;
+		return getCursorPostion(rows, cols);
 	} else {
 		*cols = ws.ws_col;
 		*rows = ws.ws_row;
