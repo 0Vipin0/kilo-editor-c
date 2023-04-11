9b8b04217795410f02eccd4b862a5ebc1800f8d1
0Vipin0 <vipinmalik941@gmail.com>
2023-04-11 21:31:06 +0530

    Step 32 - Update Cursor Position

--- kilo.c
+++ kilo.c
@@ -83,18 +83,20 @@ char editorReadKey(){
 }
 
 int getCursorPostion(int *rows, int *cols){
+	char buf[32];
+	unsigned int i = 0;
+
 	if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
 
-	printf("\r\n");
-
-	char c;
-	while (read(STDIN_FILENO, &c, 1) == 1){
-		if (iscntrl(c)){
-			printf("%d\r\n", c);
-		} else {
-			printf("%d ('%c')\r\n", c, c);
-		}
+	while (i < sizeof(buf) - 1){
+		if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
+		if (buf[i]) break;
+		i++;
 	}
+
+	buf[i] = '\0';
+
+	printf("\r\n&buf[1]: '%s'\r\n", &buf[1]);
 
 	editorReadKey();
 
