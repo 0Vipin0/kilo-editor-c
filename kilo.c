7ebb464862b6a17a2d0bb22c41400d3baf6d260a
0Vipin0 <vipinmalik941@gmail.com>
2023-04-16 16:47:26 +0530

    Step 50 - Added Page Up and Page Down Key

--- kilo.c
+++ kilo.c
@@ -20,7 +20,9 @@ enum editorKey {
 	ARROW_LEFT = 1000,
 	ARROW_RIGHT,
 	ARROW_UP,
-	ARROW_DOWN
+	ARROW_DOWN,
+	PAGE_UP,
+	PAGE_DOWN
 };
 
 /*** data ***/
@@ -98,14 +100,23 @@ int editorReadKey(){
 		if (read (STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
 
 		if (seq[0] == '['){
-			switch (seq[1]){
-				case 'A' : return ARROW_UP;
-				case 'B' : return ARROW_DOWN;
-				case 'C' : return ARROW_RIGHT;
-				case 'D' : return ARROW_LEFT;
+			if (seq[1] >= '0' && seq[1] <= '9') {
+				if (read(STDIN_FILENO, &seq[2], 1)!= 1) return '\x1b';
+				if (seq[2] == '~'){
+					switch(seq[1]){
+						case '5' : return PAGE_UP;
+						case '6' : return PAGE_DOWN;
+					}
+				}
+			} else {
+				switch (seq[1]){
+					case 'A' : return ARROW_UP;
+					case 'B' : return ARROW_DOWN;
+					case 'C' : return ARROW_RIGHT;
+					case 'D' : return ARROW_LEFT;
+				}
 			}
 		}
-		
 		return '\x1b';
 	} else {
 		return c;
